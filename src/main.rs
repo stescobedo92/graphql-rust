98f91a69677060845bb8b72e2d1ad08bbf068efd
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-05-06 20:34:10 -0400

    Add server and router for handler graphql queries

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod user_service;
 use async_graphql::{EmptyMutation, EmptySubscription};
 use async_graphql::parser::types::DirectiveLocation::Schema;
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::Router;
+use axum::routing::post;
 use crate::query_engine::Query;
 use crate::db::DB;
 
@@ -22,6 +24,12 @@ async fn graphql_handler(graph_qlrequest: GraphQLRequest) -> GraphQLResponse {
     response.into();
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/gql", post(graphql_handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening...!");
+    axum::serve(listener, app).await.unwrap()
 }
