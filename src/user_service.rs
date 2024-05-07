f6adfaf229a71a31897067e2962111c20d84dc80
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-05-06 20:19:02 -0400

    Add getter functions

--- src/user_service.rs
+++ src/user_service.rs
@@ -10,3 +10,28 @@ pub struct User {
     pub city: String,
     pub organization: String
 }
+
+#[Object]
+impl User {
+    async fn id(&self) -> &str {
+        &self.id
+    }
+    async fn name(&self) -> &str {
+        &self.name
+    }
+    async fn email(&self) -> &str {
+        &self.email
+    }
+    async fn phone(&self) -> &str {
+        &self.phone
+    }
+    async fn address(&self) -> &str {
+        &self.address
+    }
+    async fn city(&self) -> &str {
+        &self.city
+    }
+    async fn organization(&self) -> &str {
+        &self.organization
+    }
+}
